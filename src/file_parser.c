5b9c0ed1f0a029226b9e31e3e2ecb120c2c2c94e
marsp0 <martin.n.spasov@gmail.com>
2022-07-08 21:31:43 +0300

    temp commit

--- src/file_parser.c
+++ src/file_parser.c
@@ -2,18 +2,27 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 #include <string.h>
+
+static 
 
 void parse_obj(const char* file_name)
 {
-    const int LINE_SIZE = 256;
-    char line[LINE_SIZE];
-    int items;
 
     FILE* file = fopen(file_name, "r");
     assert(file != NULL);
 
-    while(fgets(line, LINE_SIZE, file) != NULL)
+    fseek(file, 0, SEEK_END);
+    int file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    unsigned char* buffer = malloc(file_size);
+    fread(buffer, sizeof(unsigned char), file_size, file);
+
+    const int LINE_SIZE = 256;
+    char line[LINE_SIZE];
+    while(fgets(line, LINE_SIZE, buffer) != NULL)
     {
         if (strncmp(line, "o ", 1) == 0)
         {
@@ -22,9 +31,5 @@ void parse_obj(const char* file_name)
     }
 
     fclose(file);
-    if (!feof(file) && ferror(file))
-    {
-        printf("Error occured while loading %s\n", file_name);
-        exit(1);
-    }
+
 }
