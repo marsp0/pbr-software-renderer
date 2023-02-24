c968ed04204f9b33b470120665f7235b3ff5f6d5
marsp0 <martin.n.spasov@gmail.com>
2023-02-24 19:41:43 +0200

    reformat comments

--- src/parsers/glb.c
+++ src/parsers/glb.c
@@ -11,23 +11,14 @@
 #include "json.h"
 #include "glb_validator.h"
 
-/*
- * GLTF 2 specification - https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html
- */
+// GLTF 2 specification - https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html
 
 static uint32_t cursor = 0;
 
 typedef struct
 {
-    uint32_t magic; /* 0x46546C67 */
-    uint32_t version;
     uint32_t size;
-} header_t;
-
-typedef struct
-{
-    uint32_t size;
-    uint32_t type; /* 0x4E4F534A or 0x004E4942 */
+    uint32_t type; // 0x4E4F534A or 0x004E4942
     const unsigned char* data;
 } chunk_t;
 
@@ -53,60 +44,57 @@ static chunk_t parse_chunk(const unsigned char* buffer)
 
     cursor += chunk.size;
 
-    /* assert that chunk is either bin or json */
+    // assert that chunk is either bin or json
     assert(chunk.type == 0x4E4F534A || chunk.type == 0x004E4942);
 
     return chunk;
 }
 
-static header_t parse_header(const unsigned char* buffer)
+static void parse_header(const unsigned char* buffer)
 {
-    header_t header;
-    header.magic = parse_int(buffer, 4);
-    header.version = parse_int(buffer, 4);
-    header.size = parse_int(buffer, 4);
+    uint32_t magic = parse_int(buffer, 4);
+    uint32_t version = parse_int(buffer, 4);
+    cursor += 4;
 
-    assert(header.version == 2);
-    assert(header.magic == 0x46546C67);
-
-    return header;
+    assert(magic == 0x46546C67);
+    assert(version == 2);
 }
 
 mesh_t* parse_scene(const char* file_name)
 {
 
-    /* open file */
+    // open file
     FILE* file = fopen(file_name, "r");
     assert(file != NULL);
 
-    /* get file size */
+    // get file size
     fseek(file, 0, SEEK_END);
     uint32_t file_size = (uint32_t)ftell(file);
     fseek(file, 0, SEEK_SET);
 
-    /* copy file contents into buffer */
+    // copy file contents into buffer
     unsigned char* buffer = malloc(file_size);
     fread(buffer, sizeof(unsigned char), file_size, file);
 
-    /* close file as its no longer needed */
+    // close file as its no longer needed
     fclose(file);
 
     printf("Allocating %.02fMB for %s\n", file_size / 1024.0 / 1024.0, file_name);
 
     cursor = 0;
-    /*TODO: do we need this header ? */
-    header_t header = parse_header(buffer);
-    printf("%d\n", header.size);
-    chunk_t json_chunk = parse_chunk(buffer);
-    /*chunk_t binary_chunk = parse_chunk(buffer);*/
-    json_t* json = json_new(json_chunk.data, json_chunk.size);
 
+    parse_header(buffer);
+
+    // process json
+    chunk_t json_chunk = parse_chunk(buffer);
+    json_t* json = json_new(json_chunk.data, json_chunk.size);
     validate_glb(json);
+
+    // chunk_t binary_chunk = parse_chunk(buffer);
 
     mesh_t* result = NULL;
 
-    /* free the buffer */
-    free(json);
+    json_free(json);
     free(buffer);
 
     return result;
