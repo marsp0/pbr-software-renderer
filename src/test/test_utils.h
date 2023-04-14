6f3ab19e091130127972a2c67da42ba296b25f01
marsp0 <martin.n.spasov@gmail.com>
2023-04-14 11:43:23 +0300

    removing printouts from assertions

--- src/test/test_utils.h
+++ src/test/test_utils.h
@@ -16,11 +16,11 @@ void assert_pointer(const void* expected,const void* actual, const char* file_na
 void assert_uint(uint32_t expected, uint32_t actual, bool is_hex, const char* file_name, int32_t line_number);
 void assert_bool(bool expected, bool actual, const char* file_name, int32_t line_number);
 
-#define ASSERT_VEC(expected, actual) assert_vec(expected, actual, "", __FILE__, __LINE__);
-#define ASSERT_FLOAT(expected, actual) assert_float(expected, actual, "", __FILE__, __LINE__);
-#define ASSERT_INT(expected, actual) assert_int(expected, actual, "", __FILE__, __LINE__);
-#define ASSERT_MAT(expected, actual) assert_mat(expected, actual, "", __FILE__, __LINE__);
-#define ASSERT_COL(expected, actual) assert_col(expected, actual, "", __FILE__, __LINE__);
+#define ASSERT_VEC(expected, actual) assert_vec(expected, actual, __FILE__, __LINE__);
+#define ASSERT_FLOAT(expected, actual) assert_float(expected, actual, __FILE__, __LINE__);
+#define ASSERT_INT(expected, actual) assert_int(expected, actual, __FILE__, __LINE__);
+#define ASSERT_MAT(expected, actual) assert_mat(expected, actual, __FILE__, __LINE__);
+#define ASSERT_COL(expected, actual) assert_col(expected, actual, __FILE__, __LINE__);
 #define ASSERT_STRING(expected, actual, size) assert_string(expected, actual, size, __FILE__, __LINE__);
 #define ASSERT_POINTER(expected, actual) assert_pointer(expected, actual, __FILE__, __LINE__);
 #define ASSERT_UINT(expected, actual) assert_uint(expected, actual, false, __FILE__, __LINE__);
