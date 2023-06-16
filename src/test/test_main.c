a96104f80e2031e62a4849ccdac208468abd2755
marsp0 <martin.n.spasov@gmail.com>
2023-06-16 14:55:07 +0300

    removing color type - uint32_t / unsigned chars will be used as colors for now

--- src/test/test_main.c
+++ src/test/test_main.c
@@ -1,6 +1,5 @@
 #include "test_vector.h"
 #include "test_matrix.h"
-#include "test_color.h"
 #include "test_json.h"
 #include "test_png.h"
 #include "test_crc.h"
@@ -11,7 +10,6 @@ int main()
 {
     test_vector();
     test_matrix();
-    test_color();
     test_json();
     test_png();
     test_crc();
--- src/test/test_utils.h
+++ src/test/test_utils.h
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 
 #include "../math.h"
-#include "../color.h"
 
 void assert_vec(vec_t expected,
                 vec_t actual,
@@ -24,11 +23,6 @@ void assert_int(int64_t expected,
 
 void assert_mat(mat_t expected,
                 mat_t actual,
-                const char* file_name,
-                int32_t line_number);
-
-void assert_col(color expected,
-                color actual,
                 const char* file_name,
                 int32_t line_number);
 
@@ -64,7 +58,6 @@ void assert_bool(bool expected,
 #define ASSERT_INT(expected, actual) assert_int((int64_t)expected, (int64_t)actual, 0, __FILE__, __LINE__);
 #define ASSERT_INT_NEAR(expected, actual, delta) assert_int((int64_t)expected, (int64_t)actual, (int64_t)delta, __FILE__, __LINE__);
 #define ASSERT_MAT(expected, actual) assert_mat(expected, actual, __FILE__, __LINE__);
-#define ASSERT_COL(expected, actual) assert_col(expected, actual, __FILE__, __LINE__);
 #define ASSERT_STRING(expected, actual, size) assert_string(expected, actual, size, __FILE__, __LINE__);
 #define ASSERT_POINTER(expected, actual) assert_pointer(expected, actual, __FILE__, __LINE__);
 #define ASSERT_POINTER_EXISTS(pointer) assert_pointer_exists(pointer, __FILE__, __LINE__);
