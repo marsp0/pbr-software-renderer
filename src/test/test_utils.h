1ffbd52554feb0e595151896f835f76a210d4539
marsp0 <martin.n.spasov@gmail.com>
2023-07-28 16:08:08 +0300

    adjusting spaces in test_utils.h

--- src/test/test_utils.h
+++ src/test/test_utils.h
@@ -28,31 +28,39 @@ void        TESTS_SUMMARY();
                                 float:      "%010f  != %010f  (%20s != %-20s)\n", \
                                 bool:       "%010d  != %010d  (%20s != %-20s)\n")
 
-#define ASSERT_EQUAL(a, b)  do \
-                            { \
-                                bool fail = GET_COMPARISON(a, b); \
-                                if (fail) \
-                                { \
-                                    increment_test_assert_counter(); \
-                                } \
-                                if (fail && get_test_assert_counter() <= 10) \
-                                { \
-                                    printf("\t\tAssert fail: "); \
-                                    printf(GET_FORMAT(a), a, b, #a, #b); \
-                                } \
-                            } while(0);
 
-#define ASSERT_TRUE(a)  do \
-                        { \
-                            ASSERT_EQUAL((a), true); \
-                        } while(0);
+#define ASSERT_EQUAL(a, b)          do \
+                                    { \
+                                        bool fail = GET_COMPARISON(a, b); \
+                                        if (fail) \
+                                        { \
+                                            increment_test_assert_counter(); \
+                                        } \
+                                        if (fail && get_test_assert_counter() <= 10) \
+                                        { \
+                                            printf("\t\tAssert fail: "); \
+                                            printf(GET_FORMAT(a), a, b, #a, #b); \
+                                        } \
+                                    } while(0);
 
-#define ASSERT_FALSE(a) do \
-                        { \
-                            ASSERT_EQUAL((a), false); \
-                        } while(0);
 
-#define ASSERT_POINTER(a, b) ASSERT_EQUAL((int64_t)a, (int64_t)b)
+#define ASSERT_TRUE(a)              do \
+                                    { \
+                                        ASSERT_EQUAL((a), true); \
+                                    } while(0);
+
+
+#define ASSERT_FALSE(a)             do \
+                                    { \
+                                        ASSERT_EQUAL((a), false); \
+                                    } while(0);
+
+
+#define ASSERT_POINTER(a, b)        do \
+                                    { \
+                                        ASSERT_EQUAL((int64_t)a, (int64_t)b); \
+                                    } while(0);
+
 
 #define ASSERT_STRING(a, b, size)   do \
                                     { \
@@ -62,38 +70,42 @@ void        TESTS_SUMMARY();
                                         } \
                                     } while(0);
 
-#define ASSERT_VECTOR(a, b) do \
-                            { \
-                                ASSERT_EQUAL(a.x, b.x); \
-                                ASSERT_EQUAL(a.y, b.y); \
-                                ASSERT_EQUAL(a.z, b.z); \
-                            } while(0);
 
-#define ASSERT_MATRIX(a, b) do \
-                            { \
-                                for (uint32_t i = 0; i < 4; i++) \
-                                { \
-                                    for (uint32_t j = 0; j < 4; j++) \
+#define ASSERT_VECTOR(a, b)         do \
                                     { \
-                                        ASSERT_EQUAL(a.data[i][j], b.data[i][j]); \
-                                    } \
-                                } \
-                            } while(0);
+                                        ASSERT_EQUAL(a.x, b.x); \
+                                        ASSERT_EQUAL(a.y, b.y); \
+                                        ASSERT_EQUAL(a.z, b.z); \
+                                    } while(0);
 
-#define TEST_CASE(test) do \
-                        { \
-                            printf("\t%s\n", #test); \
-                            reset_test_assert_counter(); \
-                            test(); \
-                            uint32_t count = get_test_assert_counter(); \
-                            if (count > 10) \
-                            { \
-                                printf("\t\t ... %u more assert fails\n", count - 10); \
-                            } \
-                        } while(0);
 
-#define TEST_GROUP(group)   do \
-                            { \
-                                printf("%s\n", #group); \
-                                group(); \
-                            } while(0);
+#define ASSERT_MATRIX(a, b)         do \
+                                    { \
+                                        for (uint32_t i = 0; i < 4; i++) \
+                                        { \
+                                            for (uint32_t j = 0; j < 4; j++) \
+                                            { \
+                                                ASSERT_EQUAL(a.data[i][j], b.data[i][j]); \
+                                            } \
+                                        } \
+                                    } while(0);
+
+
+#define TEST_CASE(test)             do \
+                                    { \
+                                        printf("\t%s\n", #test); \
+                                        reset_test_assert_counter(); \
+                                        test(); \
+                                        uint32_t count = get_test_assert_counter(); \
+                                        if (count > 10) \
+                                        { \
+                                            printf("\t\t ... %u more assert fails\n", count - 10); \
+                                        } \
+                                    } while(0);
+
+
+#define TEST_GROUP(group)           do \
+                                    { \
+                                        printf("%s\n", #group); \
+                                        group(); \
+                                    } while(0);
