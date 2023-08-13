1971a43691123a53914c44401a81ab19c00cf008
marsp0 <martin.n.spasov@gmail.com>
2023-08-13 12:54:35 +0300

    adding rasterization test for triangles where the second triangle is farther away than first

--- src/test/test_rasterizer.c
+++ src/test/test_rasterizer.c
@@ -389,7 +389,7 @@ static void test_rasterize_triangle()
                          0xAABBCCFF);
 }
 
-static void test_rasterize_multiple_triangles()
+static void test_rasterize_multiple_triangles_in_order()
 {
     // +--------------------+
     // /                    /
@@ -423,7 +423,7 @@ static void test_rasterize_multiple_triangles()
         }
     }
 
-    // rasterize first triangle
+    // rasterize first triangle (far from camera)
     rasterize_triangle(p1, p2, p3, color1, framebuffer, depthbuffer);
 
     p1 = vec_new(6.f, 6.f, 0.6f);
@@ -486,6 +486,114 @@ static void test_rasterize_multiple_triangles()
                          vec_new(6.f, 4.f, 0.6f),
                          vec_new(6.f, 5.f, 0.6f),
                          vec_new(6.f, 6.f, 0.6f)};
+
+    for (uint32_t i = 0; i < sizeof(points2) / sizeof(vec_t); i++)
+    {
+        vec_t p = points2[i];
+        ASSERT_EQUAL(color2, framebuffer_get(framebuffer, (uint32_t)p.x, (uint32_t)p.y));
+    }
+
+    framebuffer_free(framebuffer);
+    depthbuffer_free(depthbuffer);
+}
+
+static void test_rasterize_multiple_triangles_in_reverse_order()
+{
+    // +--------------------+
+    // /                    /
+    // /                    /
+    // /                    /
+    // /      xx    xx      /
+    // /      xxxx  xx      /
+    // /      xxxxxxxx      /
+    // /    xxxxxxxxxxxx    /
+    // /    xxxxxxxxxxxx    /
+    // /  xxxxxxxxxxxxxxxx  /
+    // /                    /
+    // +--------------------+
+
+    uint32_t width = 10;
+    uint32_t height = 10;
+    framebuffer_t* framebuffer = framebuffer_new(width, height);
+    depthbuffer_t* depthbuffer = depthbuffer_new(width, height);
+    vec_t p1        = vec_new(1.f, 1.f, 0.6f);
+    vec_t p2        = vec_new(8.f, 1.f, 0.6f);
+    vec_t p3        = vec_new(3.f, 6.f, 0.6f);
+    uint32_t color1 = 0xAABBCCFF;
+
+    // assert initial values
+    for (uint32_t i = 0; i < width; i++)
+    {
+        for (uint32_t j = 0; j < height; j++)
+        {
+            ASSERT_EQUAL(0, framebuffer_get(framebuffer, i, j));
+            ASSERT_EQUAL(0.f, depthbuffer_get(depthbuffer, i, j));
+        }
+    }
+
+    // rasterize first triangle (closer to camera)
+    rasterize_triangle(p1, p2, p3, color1, framebuffer, depthbuffer);
+
+    p1 = vec_new(6.f, 6.f, 0.5f);
+    p2 = vec_new(6.f, 1.f, 0.5f);
+    p3 = vec_new(8.f, 1.f, 0.5f);
+    uint32_t color2 = 0x11223344;
+
+    // rasterize second triangle (far from camera)
+    rasterize_triangle(p1, p2, p3, color2, framebuffer, depthbuffer);
+
+    // assert pixel count that has changed
+    uint32_t actual_count = 0;
+    uint32_t expected_count = 116; // 29 pixels * 4 channels
+
+    for (uint32_t i = 0; i < width * height * RGB_CHANNELS; i++)
+    {
+        if (framebuffer->data[i] != 0)
+        {
+            actual_count++;
+        }
+    }
+
+    ASSERT_EQUAL(expected_count, actual_count);
+
+    // assert first triangle
+    vec_t points[25] = {vec_new(1.f, 1.f, 0.5f),
+                        vec_new(2.f, 1.f, 0.5f),
+                        vec_new(3.f, 1.f, 0.5f),
+                        vec_new(4.f, 1.f, 0.5f),
+                        vec_new(5.f, 1.f, 0.5f),
+                        vec_new(2.f, 2.f, 0.5f),
+                        vec_new(3.f, 2.f, 0.5f),
+                        vec_new(4.f, 2.f, 0.5f),
+                        vec_new(5.f, 2.f, 0.5f),
+                        vec_new(2.f, 3.f, 0.5f),
+                        vec_new(3.f, 3.f, 0.5f),
+                        vec_new(4.f, 3.f, 0.5f),
+                        vec_new(5.f, 3.f, 0.5f),
+                        vec_new(3.f, 4.f, 0.5f),
+                        vec_new(4.f, 4.f, 0.5f),
+                        vec_new(5.f, 4.f, 0.5f),
+                        vec_new(3.f, 5.f, 0.5f),
+                        vec_new(4.f, 5.f, 0.5f),
+                        vec_new(3.f, 6.f, 0.5f),
+                        vec_new(6.f, 1.f, 0.6f),
+                        vec_new(7.f, 1.f, 0.6f),
+                        vec_new(8.f, 1.f, 0.6f),
+                        vec_new(6.f, 2.f, 0.6f),
+                        vec_new(7.f, 2.f, 0.6f),
+                        vec_new(6.f, 3.f, 0.6f)};
+
+    for (uint32_t i = 0; i < sizeof(points) / sizeof(vec_t); i++)
+    {
+        vec_t p = points[i];
+        ASSERT_EQUAL(color1, framebuffer_get(framebuffer, (uint32_t)p.x, (uint32_t)p.y));
+    }
+
+    // assert second triangle
+    vec_t points2[4] = {vec_new(7.f, 3.f, 0.6f),
+                        vec_new(6.f, 4.f, 0.6f),
+                        vec_new(6.f, 5.f, 0.6f),
+                        vec_new(6.f, 6.f, 0.6f)};
 
     for (uint32_t i = 0; i < sizeof(points2) / sizeof(vec_t); i++)
     {
@@ -569,7 +677,8 @@ void test_rasterizer()
     TEST_CASE(test_rasterize_line_pos_slope);
     TEST_CASE(test_rasterize_line_neg_slope);
     TEST_CASE(test_rasterize_triangle);
-    TEST_CASE(test_rasterize_multiple_triangles);
+    TEST_CASE(test_rasterize_multiple_triangles_in_order);
+    TEST_CASE(test_rasterize_multiple_triangles_in_reverse_order);
     TEST_CASE(test_rasterize_clipped_triangle);
     TEST_CASE(test_rasterize_colinear_triangle);
 }
