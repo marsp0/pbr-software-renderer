d5078d086c7db5eefe424c67318de8ae925d8390
marsp0 <martin.n.spasov@gmail.com>
2023-10-27 15:02:57 +0300

    adding atomic_types.h

---
+++ src/atomic_types.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdatomic.h>
+#include <stdbool.h>
+
+typedef _Atomic bool        atomic_bool_t;
+typedef _Atomic uint32_t    atomic_uint32_t;
+typedef _Atomic int32_t     atomic_int32_t;
