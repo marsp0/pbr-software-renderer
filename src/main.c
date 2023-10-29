2296dc8a31338919d5b27d156108491173f7bb19
marsp0 <martin.n.spasov@gmail.com>
2023-10-29 12:17:42 +0200

    adding rasterizer init/free calls in main

--- src/main.c
+++ src/main.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 
 #include "renderer.h"
+#include "rasterizer.h"
 #include "time_utils.h"
 
 int32_t main()
 {
     time_init();
+    rasterizer_init();
 
     renderer_t* renderer = renderer_new(800, 600, "./assets/WaterBottle.glb");
 
     renderer_run(renderer);
 
     renderer_free(renderer);
+    rasterizer_free();
 
     return 0;
 }
